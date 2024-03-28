7cbfadedac287b40dedab5444b2c7cfbb3275f0d
karan Soni <154977570+karansoni987@users.noreply.github.com>
2024-03-28 11:15:29 +0530

    useNavigate

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,18 @@
+import { Link, useNavigate } from 'react-router-dom';
 function Home() {
+    const navigate = useNavigate();
+    const navToPage = (url) => {
+        navigate(url)
+    }
     return (
-        <h1>Home Page</h1>
+        <div>
+            <h1>Home Page</h1>
+            <br />
+            <button onClick={() => navToPage('/about')}> Go to About Page</button>
+            <br /><br />
+            <button onClick={() => navToPage('/filter')}>Go to Fitler Page</button>
+
+        </div>
     )
 }
 export default Home;
